518cdeeab56af923b75e9b4e3d723eb774ad418e
alex <alexstepanyak@gmail.com>
2024-04-25 22:35:35 +0300

    note repo impl

---
+++ src/repositories/note.repo.js
@@ -0,0 +1,14 @@
+const database = require('../db/database');
+
+function createNote(newNote) {
+    return new Promise((resolve, reject) => {
+        const query = 'INSERT INTO notes SET ?';
+        database.sql.query(query, newNote, (error, results) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(results);
+            }
+        })
+    });
+}
